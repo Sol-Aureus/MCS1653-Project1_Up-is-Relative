f8a2fb16283da07c6597ceb36d09e29f61e2cb30
Sol-Aureus <ejhpepper@gmail.com>
2025-02-14 21:37:41 -0500

    Added platform tracks
    - Added tracks to visualise where the platform will go
    - Lives now show how much you lost

--- Assets/Scripts/LivesCounter.cs
+++ Assets/Scripts/LivesCounter.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class LivesCounter : MonoBehaviour
 {
     private float imageWidth = 100;
+    private float imageOffset = -50;
     public int maxLives;
     private int lives;
     private RectTransform rect;
     [SerializeField] GameObject uiScripts;
+    [SerializeField] GameObject antiLife;
+    private RectTransform antiLifeRect;
 
     // Start is called before the first frame update
     void Start()
     {
         rect = GetComponent<RectTransform>();
+        antiLifeRect = antiLife.GetComponent<RectTransform>();
         ResetLives();
     }
 
@@ -40,6 +44,11 @@ public class LivesCounter : MonoBehaviour
 
     private void SetLifeDisplay()
     {
+        // Displays remaining and lost lives
         rect.sizeDelta = new Vector2(imageWidth * lives, rect.sizeDelta.y);
+        rect.anchoredPosition = new Vector2((imageOffset * lives) - 50, rect.anchoredPosition.y);
+
+        antiLifeRect.sizeDelta = new Vector2(imageWidth * (maxLives - lives), rect.sizeDelta.y);
+        antiLifeRect.anchoredPosition = new Vector2((-imageOffset * (maxLives - lives)) - 350, rect.anchoredPosition.y);
     }
 }
